9eaf2155adf60b6dce136d383998e9f6231ffcb2
Mihail Maravelias <michhali@Mihails-MacBook-Air.local>
2026-01-25 17:50:18 -0800

    GREEN: Rectangle stores constructor values

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,14 +1,30 @@
 public class Rectangle {
+
+    private int x;
+    private int y;
     private int width;
     private int height;
 
-    public Rectangle(int w, int h) {
-        width = w;
-        height = h;
+    public Rectangle(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
-    public int area() {
-        return width * height;
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
-
--- tests/RectangleTest.java
+++ tests/RectangleTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RectangleTest {
 
     @Test
-    void testArea() {
-        Rectangle r = new Rectangle(2, 3);
-        assertEquals(6, r.area());
+    void customRectangleStoresValues() {
+        Rectangle r = new Rectangle(5, 5, 10, 10);
+
+        assertEquals(5, r.getX());
+        assertEquals(5, r.getY());
+        assertEquals(10, r.getWidth());
+        assertEquals(10, r.getHeight());
     }
 }
